3ce8a991615d7ad631dbc9253bae3e6e4140c4e8
Literou8860 <philemondefleurian@gmail.com>
2025-01-04 18:14:45 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,43 @@
+basic.forever(function () {
+    basic.showString("" + (input.compassHeading()))
+})
+basic.forever(function () {
+    if (true) {
+    	
+    }
+})
+basic.forever(function () {
+    if (true) {
+    	
+    }
+})
+basic.forever(function () {
+    if (true) {
+    	
+    }
+})
+basic.forever(function () {
+    if (true) {
+    	
+    }
+})
+basic.forever(function () {
+    if (true) {
+    	
+    }
+})
+basic.forever(function () {
+    if (true) {
+    	
+    }
+})
+basic.forever(function () {
+    if (true) {
+    	
+    }
+})
+basic.forever(function () {
+    if (true) {
+    	
+    }
+})
